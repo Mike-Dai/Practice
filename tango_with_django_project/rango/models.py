c3187eebb42ccb609049c58ed14449c7bf7a4ed6
Mike-Dai <43002285+Mike-Dai@users.noreply.github.com>
2019-01-28 21:19:22 +0800

    Add files via upload

---
+++ tango_with_django_project/rango/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField()
+	likes = models.IntegerField()
+
+	def __unicode__(self):
+		return self.name
+
+class Page(models.Model):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.title
---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+	context_dict = {'name': "Deviant Art by LizzyChrome"}
+	return render(request, 'rango/about.html', context_dict)
