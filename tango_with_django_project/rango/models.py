a8706a5ef7f3a87162c1bb01833de2cb0ad07fb8
Mike-Dai <43002285+Mike-Dai@users.noreply.github.com>
2019-01-30 17:07:21 +0800

    Add user authentication

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
@@ -25,3 +26,11 @@ class Page(models.Model):
 
 	def __unicode__(self):
 		return self.title
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='proflie_images',blank=True)
+
+	def __unicode__(self):
+		return self.user.username
