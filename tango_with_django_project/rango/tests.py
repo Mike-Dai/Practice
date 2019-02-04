aa9da09503bd779e36378a9eba5a12c3dd28980e
Michael Dai <mrmikedai@outlook.com>
2019-02-04 09:06:19 +0800

    Add tests

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -2,5 +2,49 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from rango.models import Category, Page
 
 # Create your tests here.
+
+
+def add_cat(name, views, likes):
+		c = Category.objects.get_or_create(name=name)[0]
+		c.views = views
+		c.likes = likes
+		c.save()
+		return c
+
+
+class CategoryMethodTests(TestCase):
+	def test_ensure_views_are_positive(self):
+		cat = Category(name='test', views=1, likes=0)
+		cat.save()
+		self.assertEqual((cat.views >= 0), True)
+
+	"""
+	def test_slug_line_creation(self):
+		cat = Category('Random Category String')
+		cat.save()
+		self.assertEqual(cat.slug, 'random-category-string')
+	"""
+"""
+class PageMethodTests(TestCase):
+	def test_ensure_visits_are_not_in_the_future(self):
+		page = Page()
+"""
+class IndexViewTests(TestCase):
+
+	def test_index_view_with_no_categories(self):
+		add_cat('test', 1, 1)
+		add_cat('temp', 1, 1)
+		add_cat('tmp', 1, 1)
+		add_cat('tmp test temp', 1, 1)
+
+		response = self.client.get(reverse('index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "tmp test temp")
+
+		num_cats = len(response.context['categories'])
+		self.assertEqual(num_cats, 4)
